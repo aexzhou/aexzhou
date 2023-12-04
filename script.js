baec4ee209e3ce73a4a3cb97feedb1ae931ab973
aexzhou <126853855+aexzhou@users.noreply.github.com>
2023-12-04 14:24:42 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let lastScrollTop = 0;
+navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop) {
+        navbar.style.height = "50px";
+    } else {
+        navbar.style.height = "100px";
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+}, false);
+
+navbar.addEventListener("mouseover", function() {
+    navbar.style.height = "100px";
+});
+
+navbar.addEventListener("mouseout", function() {
+    if(window.pageYOffset || document.documentElement.scrollTop > lastScrollTop){
+        navbar.style.height = "50px";
+    }
+});
